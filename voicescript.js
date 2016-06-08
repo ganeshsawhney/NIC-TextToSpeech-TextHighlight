d6798076d938963f9874c66f5a77f6dc6758a6cf
Ganesh Sawhney <ganeshsawhney@gmail.com>
2016-06-09 04:08:42 +0530

    highlighting

--- voicescript.js
+++ voicescript.js
@@ -1,33 +1,37 @@
-$(document).ready(function(){
+$(document).ready(function() {
+	$("#radio-option-1").click(function()
+	{
+		if($("#radio-option-1").is(':checked'))
+		{
+			$('#test').html('');
+		}
+
+		var str = $('.WordSection1').text();
+		var spans = str.split('।');
+
+		var gtr="";
+		var t=0;
+
+		var refreshIntervalId = setInterval(function(){
+
+			if(responsiveVoice.isPlaying() == false && $("#radio-option-1").is(':checked'))
+			{
+				var gg = spans[t];
+				t=t+1;
+				gtr=gg.replace(/`/g, '\'').replace ('\r', '').replace('\n', '');spans[t-1].replace('\n', '1').replace(' ', '2').split('\n\n').join('\n')
+
+				responsiveVoice.speak(gtr, "Hindi Female");
+				$('#test').html($('#test').html()+spans[t-1].split('\n\n').join('<br><br>')+' । ');
+			}
+
+			if($("#radio-option-1").is(':checked')==false)
+			{
+				clearInterval(refreshIntervalId);
+			}
+
+	    }, 100);
 
 
 
-alert($('.WordSection1').text());
-
-
-/*
-    $("p").click(function(){
-        alert("The paragraph was clicked.");
-    });
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	});
 });
