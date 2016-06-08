92a22ea573f2ff35edd1af18538f5db0ecb04e76
Ganesh Sawhney <ganeshsawhney@gmail.com>
2016-06-09 00:18:41 +0530

    Added ResponsiveVoice and custom script

---
+++ voicescript.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+
+
+
+alert($('.WordSection1').text());
+
+
+/*
+    $("p").click(function(){
+        alert("The paragraph was clicked.");
+    });
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
